eefee29991683a6356b02655b0c5d9bdca386693
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-09 09:52:06 +0300

    add bar

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import "./Item.css";
 
 
 
 const Item = ({card}) => {
+  const handleClick = () => {
+    setShowLogo(!showLogo)
+  }
   const {name, img, options} = card;
+  const [showLogo, setShowLogo] = useState(true)
   return (
-    <div className="card">
-      <img className="card-logo" src={img} alt="item" />
-      <h3 className="card-title">{name}</h3>
+    <div className="card" onClick={handleClick}>
+      {
+        showLogo ? (<div>
+          <img className="card-logo" src={img} alt="item" />
+        <h3 className="card-title">{name}</h3>
+        </div>) : 
+        <ul className="list">
+        {
+          options.map((element, index)=> {
+            return <li key={index}>{element}</li>;
+          })
+        }
+      </ul>
+      }
+      
+      
+      
     </div>
   )
 }
